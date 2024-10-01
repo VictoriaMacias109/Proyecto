2aa7f771da0f2b93f77bcf805ea58927c761dbea
TOSHIBA <TOSHIBA@DESKTOP-R20QFPD>
2024-09-30 23:28:09 -0700

    Primer commit

---
+++ src/logica/Prueba.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+
+import presentacion.Inicial;
+
+/**
+ *
+ * @author Natal
+ */
+public class Prueba {
+    public static void main (String args[]){
+        Inicial frmInicial = new Inicial();
+        frmInicial.setVisible(true);
+    }
+    
+    
+    public void sumar(){
+
+    }
+}
+
+
